8dfcaa93fdaa0293b7e5c72e3d3010b94163ae64
nandita <nandita.vora@stackroute.in>
2022-07-26 17:20:14 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let noteList = [];
+
+let view = "grid"; //default view is grid-view
+
+function saveNote() {
+    
+}
+
+function deleteNote() {
+    
+}
+
+function clearFields() {
+    
+}
+
+function fetchNotes() {
+    
+}
+
+function loadNoteData() {
+    
+}
+
+function updateNoteData(note) {
+    
+}
+
+function createNoteCard(note) {
+    
+}
+
+function toggleView() {
+    
+}
